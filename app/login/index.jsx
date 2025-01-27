c22759d131d40671a144670d5efe09af1eef2525
Amith B V <amithrao0606@gmail.com>
2025-01-27 14:25:07 +0530

    Created a welcome page, a page which will be seen by the users, as they open the app.

---
+++ app/login/index.jsx
@@ -0,0 +1,85 @@
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
+import Colors from "../../constant/Colors";
+import { useRouter } from "expo-router";
+
+export default function LoginScreen() {
+  const router = useRouter();
+
+  return (
+    <View style={{ backgroundColor: "#FFA62B" }}>
+      {/* Image part :  */}
+      <View
+        style={{
+          display: "flex",
+          alignItems: "center",
+          marginTop: 40,
+        }}
+      >
+        <Image
+          source={require("./../../assets/images/login.png")}
+          style={styles.image}
+        />
+      </View>
+
+        {/* Text and button : */}
+      <View
+        style={{
+          padding: 25,
+          backgroundColor: Colors.PRIMARY,
+          height: "100%",
+          borderRadius: 25,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 30,
+            fontWeight: "bold",
+            color: "white",
+            textAlign: "center",
+          }}
+        >
+          Stay on Track, Stay Healthy!
+        </Text>
+
+        <Text
+          style={{
+            color: "white",
+            textAlign: "center",
+            fontSize: 17,
+            marginTop: 20
+          }}
+        >
+          Track your meds, take control of your health. Stay consistant, Stay
+          confident!
+        </Text>
+
+        <TouchableOpacity style={styles.button} onPress={() => router.push("/login/signIn")}>
+          <Text style={{ textAlign: "center", fontSize: 17 }}>Continue</Text>
+        </TouchableOpacity>
+
+        <Text
+          style={{
+            color: "white",
+            marginTop: 17,
+            textAlign: "center",
+            fontSize: 9
+          }}
+        >Note : By clicking Continue button, you are argreeing to our Terms & Condition</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  image: {
+    height: 450,
+    width: 255,
+  },
+  button: {
+    padding: 15,
+    backgroundColor: "#FFA62B",
+    borderRadius: 99,
+    marginTop: 17
+  }
+});
